9796bb49ba9eda8b713b3a908caa5f2009580e9c
miguel <miguel.ceriani@gmail>
2015-04-11 18:04:16 +0200

    Backend Created

---
+++ back-end/missionTypes.js
@@ -0,0 +1,41 @@
+// var express = require('express');
+// var app = express();
+
+var fs = require('fs');
+
+function missionTypes(app, callback) {
+
+  fs.readFile(
+      'xploration-data/missions.json',
+      function (err, data) {
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        var missionTypes = JSON.parse(data.toString());
+        var missionTypesMap = {};
+        missionTypes.map( function(mt) {
+          missionTypesMap[mt.slug] = mt;
+          app.get('/missions/' + mt.slug, function (req, res) {
+            res.send(
+              JSON.stringify(missionTypesMap[mt.slug]) );
+          });
+        });
+        app.get('/missions', function (req, res) {
+          res.send(
+            JSON.stringify(
+              Object.keys(missionTypesMap).map(
+                function (mt_slug) {
+                  return mt_slug;
+                } )));
+        });
+
+
+      });
+
+  callback(null);
+
+}
+
+module.exports = missionTypes;
