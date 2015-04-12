55c85c1a5a051937dff0d6127b5604028db4f345
miguel <miguel.ceriani@gmail>
2015-04-12 13:48:37 +0200

    API /evaluate-score added

---
+++ back-end/evaluateScore.js
@@ -0,0 +1,79 @@
+var fs = require('fs');
+var _ = require("underscore");
+
+function setEvaluateScore(app, callback) {
+
+  fs.readFile(
+      'xploration-data/destinations.json',
+      function (err, data) {
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        // var missionTypes = JSON.parse(data.toString());
+        // var missionTypesMap = {};
+        // missionTypes.map( function(mt) {
+        //   missionTypesMap[mt.name] = _.extend({},mt,{id:mt.name});
+        //   app.get('/destinations/' + mt.name, function (req, res) {
+        //     res.contentType('application/json');
+        //     res.send(
+        //       JSON.stringify(missionTypesMap[mt.name]) );
+        //   });
+        // });
+        // var missionTypesMapStr =
+        //   JSON.stringify(
+        //     Object.keys(missionTypesMap).map(
+        //       function (mt_slug) {
+        //         return missionTypesMap[mt_slug];
+        //       } ));
+
+        app.post('/evaluate-score', function (req, res) {
+          var input = req.body;
+
+          // var input = {
+          //   //components: [],
+          //   payload: {id: "A"},
+          //   fundamentalComponents: [{id:"A"}, {id:"B"}],
+          //   scientificInterest: 2000, // 1000-5000
+          //   mass: { value: 250, max: 1000},
+          //   volume: { value: 250, max: 1000},
+          //   power: { value: 250, max: 1000},
+          //   temperature: { valueMin: 7, valueMax: 28, min: -15, max: 20}
+          // };
+
+          var score = 0;
+
+          if (  input.fundamentalComponents.map(function(comp) {
+                  return comp.id;
+                }).indexOf(input.payload.id) >= 0) {
+            var temp = input.temperature;
+            var temperatureScore =
+                ( temp.valueMin - temp.min < 5
+                  || temp.max - temp.valueMax < 5) ?
+                      -2000:
+                      500;
+            score +=
+              4500 * input.power.value / input.power.max
+              + 3000 * input.volume.value / input.volume.max
+              + 1500 * input.mass.value / input.mass.max
+              + input.scientificInterest
+              + temperatureScore;
+          }
+
+          var output = {
+            score: score
+          };
+
+          res.contentType('application/json');
+          res.send(JSON.stringify(output));
+        });
+
+
+      });
+
+  callback(null);
+
+}
+
+module.exports = setEvaluateScore;
--- back-end/index.js
+++ back-end/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 var fs = require('fs');
+var bodyParser = require('body-parser')
 var missionTypes = require('./missionTypes');
 var destinations = require('./destinations');
 var componentTypes = require('./componentTypes');
+var setEvaluateScore = require('./evaluateScore');
 
 var apis = [
   { id: 'missionTypes',
@@ -12,9 +14,14 @@ var apis = [
   { id: 'destinations',
     descr: 'Available mission destinations' },
     { id: 'subsystems',
-      descr: 'Available types of subsystems' }
+      descr: 'Available types of subsystems' },
+      { id: 'evaluate-score',
+        descr: 'Evaluate the score of a cube-sat' }
   ];
 var apiStr = JSON.stringify(apis);
+
+var app = express();
+app.use(bodyParser());
 
 missionTypes(app, function(err) {
   if (err) { console.err(err); return; }
@@ -25,20 +32,24 @@ missionTypes(app, function(err) {
     componentTypes(app, function(err) {
       if (err) { console.err(err); return; }
 
-      app.get('/', function (req, res) {
-        res.contentType('application/json');
-        res.send(apiStr);
+      setEvaluateScore(app, function(err) {
+        if (err) { console.err(err); return; }
+
+        app.get('/', function (req, res) {
+          res.contentType('application/json');
+          res.send(apiStr);
+        });
+
+        var server = app.listen(3000, function () {
+
+          var host = server.address().address;
+          var port = server.address().port;
+
+          console.log('Back-end app listening at http://%s:%s', host, port);
+
+        });
+
       });
-
-      var server = app.listen(3000, function () {
-
-        var host = server.address().address;
-        var port = server.address().port;
-
-        console.log('Example app listening at http://%s:%s', host, port);
-
-      });
-
     });
 
   });
